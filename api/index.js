651101d701052be8a66f490f4aa8917f82943c32
Miguel Machado <miiguellmp@gmail.com>
2022-12-21 11:04:32 -0300

    feat: conectando com dados

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const userSchema = require("./user/schema/user.graphql");
+const userResolvers = require("./user/resolvers/userResolvers");
+
+const typeDefs = [userSchema];
+const resolvers = [userResolvers];
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      userAPI: new UserAPI(),
+    };
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Servidor rodando na porta ${url}`);
+});
---
+++ api/user/datasource/user.js
@@ -0,0 +1,16 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class UserAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://localhost:3000/';
+  }
+
+  async getUsers() {
+    return this.get('users');
+  }
+
+  async getUser(id) {
+    return this.get(`users/${id}`);
+  }
+}
